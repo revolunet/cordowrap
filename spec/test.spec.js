f4244dd2aed990df564b9b07cdb935318b970b81
Julien Bouquillon <julien@bouquillon.com>
2015-07-18 23:33:03 +0200

    add tests

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,8 +1,47 @@
 'use strict';
 
-var test = require('tape');
+let test = require('tape');
+let CordoWrap = require('../src');
 
-test("should work", (t) => {
-	t.equal(1, 1);
-	t.end();
+let cordoWrap = () => new CordoWrap();
+
+test('Contacts.find should fail without mock', t => {
+	t.plan(1);
+	let c = cordoWrap();
+	c.plugins.Contacts.find().catch(() => {
+		t.pass('navigator.find should throw OK');
+	});
 });
+
+
+test('useDefaultMocks: Contacts.find should return default mocks', t => {
+	t.plan(1);
+	let c = cordoWrap();
+	c.useDefaultMocks();
+	let expected = require('../src/plugins/Contacts/mocks/find')();
+	c.plugins.Contacts.find().then(result=>{
+		t.deepEqual(result, expected);
+	}).catch(() => {
+		t.fail('navigator.find should NOT throw with mocks');
+	});
+});
+
+test('setMocks: Contacts.find should return custom mock', t => {
+	t.plan(1);
+	let c = cordoWrap();
+	c.setMocks({
+		Contacts: {
+			find: () => {
+				return 42;
+			}
+		}
+	});
+	c.plugins.Contacts.find().then(result=>{
+		t.equal(result, 42);
+	}).catch(() => {
+		t.fail('navigator.find should NOT throw with mocks');
+	});
+});
+
+
+//cordoWrap.plugins.Contacts.find().then(contacts => {
